78541a012a24f7bd0861d48784f1d8dfbd5601c1
jinyangTF <jinyang@tinyfish.io>
2024-02-25 10:32:51 -0800

    Frontend set up

--- backend/src/lib/api.ts
+++
@@ -1,6 +0,0 @@
-import { BskyAgent } from "@atproto/api";
-
-export const agent = new BskyAgent({
-  // App View URL
-  service: "https://api.bsky.app",
-});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <text>hello aaron</text>
+    </>
+  )
+}
+
+export default App
